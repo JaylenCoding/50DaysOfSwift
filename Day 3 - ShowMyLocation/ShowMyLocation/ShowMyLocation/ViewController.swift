c1d1c54e0b2d549aa14bc130e0c6b57ef5fc1ae9
JunLin Bian <Minecoder@163.com>
2017-06-29 21:15:21 +0800

    Project 3, use CLocation to show current location

---
+++ Day 3 - ShowMyLocation/ShowMyLocation/ShowMyLocation/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ShowMyLocation
+//
+//  Created by Minecode on 2017/6/29.
+//  Copyright © 2017年 org.minecode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Widget
+    @IBOutlet weak var showButton: UIButton!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    
+    // Data
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func showAction(_ sender: Any) {
+        
+    }
+    
+    
+}
+
