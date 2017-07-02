b6130eb7844c4aa505dc430f16832f837075c8d5
JunLin Bian <Minecoder@163.com>
2017-07-02 23:59:05 +0800

    Project 3, CLLocation

--- Day 3 - ShowMyLocation/ShowMyLocation/ShowMyLocation/ViewController.swift
+++ Day 3 - ShowMyLocation/ShowMyLocation/ShowMyLocation/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     // Widget
     @IBOutlet weak var showButton: UIButton!
@@ -16,7 +17,7 @@ class ViewController: UIViewController {
     
     
     // Data
-    
+    var locationManager: CLLocationManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,9 +26,56 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAction(_ sender: Any) {
-        
+        // 初始化地图框架
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        // 设置精度
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
     }
     
+}
+
+// 遵守CLLocationManager协议
+extension ViewController {
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+        self.locationLabel.text = "Error while updating location: " + error.localizedDescription
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // 将经纬度转化成地址
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: {
+        (placemark, error) -> Void in
+            
+            if error != nil {
+                self.locationLabel.text = "Reverse geocoder failed with error: " + error!.localizedDescription
+                return
+            }
+            
+            if placemark!.count > 0 {
+                
+            }
+            else {
+                self.locationLabel.text = "Error existed in the data received from geocoder"
+            }
+            
+        })
+    }
+    
+    func displayLocationInfo(_ placemark: CLPlacemark?) {
+        guard let containsPlacemark = placemark else {return}
+        
+        locationManager.stopUpdatingLocation()
+        
+        let locality = (containsPlacemark.locality != nil) ? containsPlacemark.locality : ""
+        let postalCode = (containsPlacemark.postalCode != nil) ? containsPlacemark.postalCode : ""
+        let adminstrativeArea = (containsPlacemark.administrativeArea != nil) ? containsPlacemark.administrativeArea : ""
+        let country = (containsPlacemark.country != nil) ? containsPlacemark.country : ""
+        
+    }
     
 }
 
