73b169430534df5363d7964ef77230f99b61f967
JunLin Bian <Minecoder@163.com>
2017-07-18 08:58:41 +0800

    项目基本配置

---
+++ Day 5 - ChannelBar/ChannelBar/ChannelBar/Classese/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChannelBar
+//
+//  Created by Minecode on 2017/7/18.
+//  Copyright © 2017年 Minecode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
