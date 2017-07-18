853cda83082aecb1a6d331681e18b50ac7b60031
JunLin Bian <Minecoder@163.com>
2017-07-18 10:00:55 +0800

    设置频道数据

--- Day 5 - ChannelBar/ChannelBar/ChannelBar/Classese/Controller/ViewController.swift
+++ Day 5 - ChannelBar/ChannelBar/ChannelBar/Classese/Controller/ViewController.swift
@@ -12,10 +12,12 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var channelView: MCChannelView!
     
+    lazy var channels: Array<String> = ["推荐", "资讯", "重庆", "社会", "军事", "娱乐", "明星", "动漫", "音乐", "影视", "政治", "历史", "二次元", "中国足球", "科技", "NBA"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.channelView.channels = channels
     }
 
 
--- Day 5 - ChannelBar/ChannelBar/ChannelBar/Classese/View/MCChannelView.swift
+++ Day 5 - ChannelBar/ChannelBar/ChannelBar/Classese/View/MCChannelView.swift
@@ -8,10 +8,46 @@
 
 import UIKit
 
+let kChannelMargin: CGFloat = 10
+
 class MCChannelView: UIView {
 
     @IBOutlet weak var scrollView: UIScrollView!
     
+    // 外界设置channel时，就会调用这个方法
+    var channels: Array<String> = Array<String>() {
+        didSet {
+            setChannels(channels: channels)
+        }
+    }
     
+    func setChannels(channels: Array<String>) {
+        
+        var offsetX: CGFloat = kChannelMargin
+        
+        for model in channels {
+            let label = UILabel()
+            
+            label.text = model
+            label.font = UIFont.systemFont(ofSize: 18)
+            label.textColor = UIColor.black
+            
+            label.sizeToFit()
+            label.font = UIFont.systemFont(ofSize: 14)
+            
+            var frame = label.frame
+            frame.origin.x = offsetX
+            frame.size.height = 35
+            label.frame = frame
+            
+            self.scrollView.addSubview(label)
+            
+            offsetX += label.frame.size.width + kChannelMargin
+        }
+        
+        // 设置滚动视图的滚动距离
+        scrollView.contentSize = CGSize(width: offsetX, height: 35)
+        
+    }
 
 }
