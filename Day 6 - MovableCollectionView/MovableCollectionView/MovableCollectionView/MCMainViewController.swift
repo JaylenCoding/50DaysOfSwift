134270c32d2df5320d89c47e78dba98953a8bc39
JunLin Bian <Minecoder@163.com>
2017-07-24 14:43:09 +0800

    Build base foundation.

---
+++ Day 6 - MovableCollectionView/MovableCollectionView/MovableCollectionView/MCMainViewController.swift
@@ -0,0 +1,70 @@
+//
+//  MCMainViewController.swift
+//  MovableCollectionView
+//
+//  Created by Minecode on 2017/7/24.
+//  Copyright © 2017年 Minecode. All rights reserved.
+//
+
+import UIKit
+
+// 枚举
+enum VC: String {
+    case ViewController
+    case CollectionViewController
+    
+    func segueIdentifier() -> String {
+        switch self {
+        case .ViewController:
+            return "VCSegueIdentifier"
+        case .CollectionViewController:
+            return "CVCSegueIdentifier"
+        }
+    }
+}
+
+let cellID: String = "CellId"
+
+class MCMainViewController: UITableViewController {
+    
+    lazy var vc: [VC] = {
+        var tmpArray: [VC] = [.ViewController, .CollectionViewController]
+        return tmpArray
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+ 
+}
+
+extension MCMainViewController {
+    
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.vc.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        cell.textLabel?.text = vc[indexPath.item].rawValue
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: vc[indexPath.item].segueIdentifier(), sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        for iter in 0..<vc.count {
+            if segue.identifier == vc[iter].segueIdentifier() {
+                segue.destination.title = vc[iter].rawValue
+                break
+            }
+        }
+        
+    }
+}
---
+++ Day 6 - MovableCollectionView/MovableCollectionView/MovableCollectionView/MCViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MCViewController.swift
+//  MovableCollectionView
+//
+//  Created by Minecode on 2017/7/24.
+//  Copyright © 2017年 Minecode. All rights reserved.
+//
+
+import UIKit
+
+class MCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
