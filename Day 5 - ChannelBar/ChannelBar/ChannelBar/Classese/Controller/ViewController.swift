d341f6fa39acd204012e5c76663e97abdd35d8c4
JunLin Bian <Minecoder@163.com>
2017-07-27 16:36:31 +0800

    Bug fixed: collection view has a white bar, layout position error, no response while slide

--- Day 5 - ChannelBar/ChannelBar/ChannelBar/Classese/Controller/ViewController.swift
+++ Day 5 - ChannelBar/ChannelBar/ChannelBar/Classese/Controller/ViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let cellId = "cellId"
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var channelView: MCChannelView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     lazy var channels: Array<String> = ["推荐", "资讯", "重庆", "社会", "军事", "娱乐", "明星", "动漫", "音乐", "影视", "政治", "历史", "二次元", "中国足球", "科技", "NBA"]
     
@@ -18,8 +21,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         self.channelView.channels = channels
+        self.automaticallyAdjustsScrollViewInsets = false
     }
 
 
 }
 
+extension ViewController {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.channels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = UIColor.arc_random_light()
+        
+        let label:UILabel = cell.viewWithTag(100) as! UILabel
+        label.text = self.channels[indexPath.item]
+        
+        return cell
+    }
+}
